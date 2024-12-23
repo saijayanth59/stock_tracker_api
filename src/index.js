3ef5f70326556edd46367f54417690eb59e167bb
saijayanth59 <sithlord5995@gmail.com>
2024-12-23 08:58:40 +0530

    feat: add cors to allow all orgins

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ const bodyparser = require("body-parser");
 const app = express();
 const { PORT } = require("./config/serverConfig");
 const connectDB = require("./config/database");
-const apiRoutes=require("./routes/index");
+const apiRoutes = require("./routes/index");
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -19,4 +27,3 @@ app.listen(PORT, async () => {
   console.log(`Server started at ${PORT}`);
   connectDB();
 });
- 
